74eeb08f432330ccc193d30e208be221f22e70b4
Jens Krüger <jkrueger@adobe.com>
2021-08-29 12:39:28 +0200

    Remove macosx_sdlmain
    I misread the SDL 2.0 transition guide (and looked into the SDL2 sources), the mac application setup isn't needed, anymore.
    However, the app can create an NSApplication and/or an NSApplicationDelegate. Also the app _can_ supply a main nib/xib file to setup the main menu.
    TODO:
    - set the working directory to the .app's parent directory if app is double-clicked in the Finder? -> SDL_DROPFILE
    - clean out -psn command line option when app is double-clicked?

--- xpilot/src/client/sdl/main.c
+++ xpilot/src/client/sdl/main.c
@@ -44,7 +44,7 @@ const char *Program_name(void)
     return "xpilot-ng-sdl";
 }
 
-int xp_main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     bool auto_shutdown = false;
     int result;
